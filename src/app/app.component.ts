ce879d113a8723f890428a59f616ffe0a9745191
Stevan <stevan14padilha@gmail.com>
2023-12-20 14:28:30 -0300

    feat: pages and home structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-import { BigCardComponent } from './components/big-card/big-card.component';
-import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
-import { MenuTitleComponent } from './components/menu-title/menu-title.component';
-import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-
-    BigCardComponent,
-    MenuBarComponent,
-    MenuTitleComponent,
-    SmallCardComponent,
-  ],
+  imports: [CommonModule, RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
